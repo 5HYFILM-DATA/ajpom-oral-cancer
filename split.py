0796d2789c452a8dbe381d4924f2a2afb65bcfc7
Waranthorn Chansawang <43036360+filmerxyz@users.noreply.github.com>
2021-01-21 14:50:57 +0700

    Create split.py

---
+++ split.py
@@ -0,0 +1,6 @@
+import splitfolders
+
+splitfolders.ratio('lip',
+                    output='/Users/waranthornchansawang/Desktop/oral_cancer/xxx',
+                    seed=1337,
+                    ratio=(0.8, 0.2)) #train=0.8, val=0.1, test=0.1
